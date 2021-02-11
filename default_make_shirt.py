7c26a31b587d7943dac9bb5ec5810db4c554334b
JakeSmith <jake.smith@argusmedia.com>
2021-02-11 12:31:38 +0000

    did some more python here

--- default_make_shirt.py
+++ default_make_shirt.py
@@ -1,7 +1,8 @@
-def make_shirt(size, text):
+def make_shirt(size='L', text='This is default text'):
     """Text of the shirt goes here"""
     print(f"this is size {size.title()}")
     print(f"{text}")
 
-make_shirt('M','Cool shirt bro')
-make_shirt(text='keyword robot', size='XXXXXXXL')
+make_shirt('M')
+make_shirt()
+make_shirt(size='XL',text='the XL one')
